d6b69d56442f14664f278ec950c4f893d04c7e8e
Huang Kai <hk@ciphergateway.com>
2017-08-15 19:38:08 +0800

    Initial commit

---
+++ src/main/java/io/nianhua100/openpcr/protocol/IStream.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2017, CipherGateway and/or its affiliates. All rights  reserved.
+ *
+ */
+package io.nianhua100.openpcr.protocol;
+
+public interface IStream {
+  public byte[] toStream();
+}
---
+++ src/main/java/io/nianhua100/openpcr/protocol/OpenPcrProtocolTail.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2017, CipherGateway and/or its affiliates. All rights  reserved.
+ *
+ */
+package io.nianhua100.openpcr.protocol;
+
+public class OpenPcrProtocolTail implements IStream{
+  public byte[] toStream() {
+    return new byte[0];
+  }
+}
